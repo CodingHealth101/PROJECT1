abaeb08cc95567b8cf464a74c96a7120aecee9fb
CodingHealth101 <isaacgwalulya@gmail.com>
2025-06-26 08:42:38 -0700

    arrays1

---
+++ arrays.js
@@ -0,0 +1,10 @@
+---
+description: 'Description of the custom chat mode.'
+tools: [ 'tool1', 'tool2' ]
+Let fruits = ['apple', 'banana', 'cherry'];
+console.log(fruits.length); // Output: 3
+
+console.log(fruits) // Output: ['apple', 'banana', 'cherry']
+fruits.push('date');
+fruits.push('elderberry');
+console.log(fruits.length); // Output: 5
